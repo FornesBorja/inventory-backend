a3148350a06f77182484000b183db6904694db57
FornesBorja <fornes.borja@gmail.com>
2024-11-27 18:16:30 +0100

    feat: read all products in rest controller done

--- src/main/java/com/fornesb/inventory/controller/ProductController.java
+++ src/main/java/com/fornesb/inventory/controller/ProductController.java
@@ -1,8 +1,16 @@
 package com.fornesb.inventory.controller;
 
+import com.fornesb.inventory.model.Product;
+import com.fornesb.inventory.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 
@@ -10,4 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(value = "http://localhost:4200")
 
 public class ProductController {
+    private static final Logger logger=LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService;
+
+    //http://localhost:8080inventory-app/products
+    @GetMapping("/products")
+    public List<Product> getProducts(){
+        List<Product> products = this.productService.listAllProducts();
+        logger.info("---------Products obtained:----------");
+        products.forEach((product -> logger.info(product.toString())));
+        return products;
+    }
+
 }
